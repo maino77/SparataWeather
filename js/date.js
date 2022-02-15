94d6b2e08d8be08032cbf01bb773986ead8ca068
maino77 <dlalsgh0812@naver.com>
2022-02-15 23:12:08 +0900

    [추가] 날씨 지역 연동 성공

---
+++ js/date.js
@@ -0,0 +1,33 @@
+const clock = document.querySelector(".clock");
+/*
+html 문서안에서 class 이름이 clock인 코드를 가지고 와서
+상수 clock으로 설정해줍니다.
+
+
+이 부분 입니다.
+*/
+
+const clockTitle = clock.querySelector("td");
+/*
+그 후  
+<div class="clock">
+    <h1>00:00:00</h1>
+</div>
+안에 있는 h1 태그를 상수 clockTitle로 설정해줍니다.
+*/
+
+function Time(){
+    const date = new Date();
+    const year = date.getFullYear(); // 연도
+    const hour = date.getHours(); // '시'를 저장
+    const minute = date.getMinutes(); // '분'을 저장
+    const second = date.getSeconds(); // '초'를 저장
+    clockTitle.innerText = `${year}년 ${hour < 12 ? "오전" : "오후"} ${hour < 10 ? `0${hour}` : hour > 12 ? `${hour-12}` : hour}시 ${minute < 10 ? `0${minute}` : minute}분 ${second < 10 ? `0${second}` : second}`;
+                                                                // hour > 12 ? `${hour-12} : hour
+}
+
+function show(){
+    Time();
+    setInterval(Time,1000);
+}
+show();
--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,73 @@
 const API_KEY = "9730934379c4806648360d3cc00d8894";
 
-// getCurrentPosition이 성공했을 때 함수
-// latitude 위도, longitude 경도
 function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    $("#selectGeo").change(function(){
+    // select로 지역 선택시 작동
+    var selectVal = $("#selectGeo option:selected").val(); // 선택한 값 가져 옴
+        console.log(selectVal);
+    if(selectVal == "현위치"){
+        const change_lat = position.coords.latitude;
+        const change_lon = position.coords.longitude;
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "서울"){
+        const change_lat = 37.583328
+        const change_lon = 127
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "강릉"){
+        const change_lat = 37.751853
+        const change_lon = 128.8760574
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "대전"){
+        const change_lat = 36.321655
+        const change_lon = 127.378953
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "대구"){
+        const change_lat = 35.798838
+        const change_lon = 128.583052
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "광주"){
+        const change_lat = 35.1757811066707
+        const change_lon = 126.870704892426
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "부산"){
+        const change_lat = 35.198362
+        const change_lon = 129.053922
+
+        lat = change_lat
+        lon = change_lon
+    } else if(selectVal == "제주"){
+        const change_lat = 33.364805
+        const change_lon = 126.542671
+
+        lat = change_lat
+        lon = change_lon
+    } else {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        return lat, lon
+    }
+        search_weather();
+    })
+
+    
+    function search_weather(){
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     // fetch함수: 서버에 데이터 요청
     // .then 서버에서 데이터를 가져오는 작업이 완료된 이후 then에 있는 함수 실행 
@@ -12,13 +75,22 @@ function onGeoOk(position){
     fetch(url)
         .then(response => response.json())
         .then(data =>{
-        const name = data.name;
-        const temp =  data.main.temp
-        const feels_like = data.main.feels_like
-        const temp_min = data.main.temp_min
-        const temp_max = data.main.temp_max
-        const humidity = data.main.humidity
+        const feels_like = document.getElementById("feel_temp_info") // 체감온도
+        const temp_min = document.getElementById("min_temp_info")  // 최저 온도
+        const temp_max = document.getElementById("max_temp_info")  // 최고 온도
+        const humidity = document.getElementById("humidity_info") // 습도
+        const cur_temp = document.getElementById("cur_temp_info") // 현재 온도
+        const city_name = document.getElementById("city_info") // 현재 온도
+        
+        city_name.innerText = data.name;
+        cur_temp.innerText = data.main.temp.toFixed(1)
+        feels_like.innerText = data.main.feels_like.toFixed(1)
+        temp_min.innerText = data.main.temp_min.toFixed(1)
+        temp_max.innerText = data.main.temp_max.toFixed(1)
+        humidity.innerText = data.main.humidity
     });
+    }
+    search_weather();
 }
 
 // 변수 정리
