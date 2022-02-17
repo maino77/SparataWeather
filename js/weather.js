476556216f3815a80e839321afc563e67d2bae71
maino77 <dlalsgh0812@naver.com>
2022-02-17 21:19:56 +0900

    [추가] 애플 로고 로딩화면 추가

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,19 @@
+
+// 로딩 페이지 js
+// https://shanepark.tistory.com/148
+$(function(){
+    
+	let loading = $('<div id="loading" class="loading"><img id="loading_img" alt="loading" src="/resources/images/loading/ajax-loader.gif" /></div>')
+			.appendTo(document.body).hide();
+	
+	$(window).ajaxStart(function(){
+		loading.show();
+	}).ajaxStop(function(){
+		loading.hide();
+	});
+});
+
+
 const API_KEY = "9730934379c4806648360d3cc00d8894";
 
 function onGeoOk(position){
